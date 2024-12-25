9d127f4e1c14c366f60c53a847f4b79d6923d2eb
Mohamed Emam <emaamm10@gmail.com>
2024-12-25 12:18:17 +0200

    Add Say hello.cpp solution

---
+++ problem solved/Say hello.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+
+int main() {
+
+    std::string name;
+
+
+    std::getline(std::cin, name);
+
+
+    std::cout << "Hello, " << name << std::endl;
+
+
+    return 0;
+}
